4970ee0becbe930bd0976f00ac9c1459de435cb3
myferr <contactme.myfer@protonmail.com>
2025-06-17 23:31:37 -0500

    feat: add utils

---
+++ src/lib/utils.ts
@@ -0,0 +1,24 @@
+function getAvailableModels() {
+  return [
+    "llama3:8b",
+    "llama2-uncensored",
+    "gemma3",
+    "gemma",
+    "phi3:mini",
+    "mistral",
+    "gemma:2b",
+    "gemma:7b",
+    "qwen:7b",
+    "qwen2.5-coder",
+    "qwen3",
+    "deepseek-coder:6.7b",
+    "deepseek-v2:16b",
+    "dolphin-mistral:7b",
+    "dolphin3",
+    "starcoder2:7b",
+    "magistral",
+    "devstral",
+  ];
+}
+
+export { getAvailableModels };
